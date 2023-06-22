91f820fdc619f5d1708d54113b52f0fa9cdd7a89
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-06-22 14:05:14 -0400

    Add functionality to decode words  and paragraph

--- decode_morse_code.rb
+++ decode_morse_code.rb
@@ -1,18 +1,31 @@
-def decode_char(char)
-  dictionary = {
-    '.-' => 'A', '-...' => 'B', '-.-.' => 'C',
-    '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G',
-    '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V',
-    '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z'
-  }.freeze
-  dictionary[char]
+# Morse Code dictionary
+MORSE_CODE = {
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C',
+  '-..' => 'D', '.'    => 'E', '..-.' => 'F',
+  '--.' => 'G', '....' => 'H', '..'   => 'I',
+  '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+  '--' => 'M', '-.' => 'N', '---' => 'O',
+  '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R',
+  '...' => 'S', '-' => 'T', '..-' => 'U',
+  '...-' => 'V', '.--'  => 'W', '-..-' => 'X',
+  '-.--' => 'Y', '--..' => 'Z'
+}.freeze
+
+def decode_char(given_char)
+  MORSE_CODE[given_char]
 end
 
-def decode_word(givenword)
-  decodedword = ''
-  array = givenword.split
-  array.each do |element|
-    decodedword += decode_char(element)
-  end
-  decodedword
+puts decode_char('.-')
+
+def decode_word(given_word)
+  given_word.split.map { |char| decode_char(char) }.join
 end
+
+puts decode_word('-- -.--')
+
+def decode(given_code)
+  given_code.split('   ').map { |word| decode_word(word) }.join(' ')
+end
+
+puts decode('-- -.--   -. .- -- .')
+puts decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
