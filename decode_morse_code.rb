3dacf79a4b82af38ea6c9a8a9cddb7b11debd45e
haftamudesta <desta451616@hotmail.com>
2023-06-22 17:33:20 +0300

    Add decode-character and decode word

---
+++ decode_morse_code.rb
@@ -0,0 +1,42 @@
+def decode_char(character)
+dictionary = {
+    '.-' => 'A',
+    '-...' => 'B',
+    '-.-.' => 'C',
+    '-..' => 'D',
+    '.' => 'E',
+    '..-.' => 'F',
+    '--.' => 'G',
+    '....' => 'H',
+    '..' => 'I',
+    '.---' => 'J',
+    '-.-' => 'K',
+    '.-..' => 'L',
+    '--' => 'M',
+    '-.' => 'N',
+    '---' => 'O',
+    '.--.' => 'P',
+    '--.-' => 'Q',
+    '.-.' => 'R',
+    '...' => 'S',
+    '-' => 'T',
+    '..-' => 'U',
+    '...-' => 'V',
+    '.--' => 'W',
+    '-..-' => 'X',
+    '-.--' => 'Y',
+    '--..' => 'Z'
+  }
+return dictionary[character]
+end
+
+def decode_word(givenWord)
+    decodedWord = ''
+    array = givenWord.split
+    array.each do |element|
+      decodedWord += decode_char(element)
+    end
+    return decodedWord
+  end
+
+  
