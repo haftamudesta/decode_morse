1d9d4de4e3ff7ad34812b19e46e5b42dca5f2cee
haftamudesta <desta451616@hotmail.com>
2023-06-22 18:26:58 +0300

    Add configurations

--- decode_morse_code.rb
+++ decode_morse_code.rb
@@ -1,42 +1,18 @@
-def decode_char(character)
-dictionary = {
-    '.-' => 'A',
-    '-...' => 'B',
-    '-.-.' => 'C',
-    '-..' => 'D',
-    '.' => 'E',
-    '..-.' => 'F',
-    '--.' => 'G',
-    '....' => 'H',
-    '..' => 'I',
-    '.---' => 'J',
-    '-.-' => 'K',
-    '.-..' => 'L',
-    '--' => 'M',
-    '-.' => 'N',
-    '---' => 'O',
-    '.--.' => 'P',
-    '--.-' => 'Q',
-    '.-.' => 'R',
-    '...' => 'S',
-    '-' => 'T',
-    '..-' => 'U',
-    '...-' => 'V',
-    '.--' => 'W',
-    '-..-' => 'X',
-    '-.--' => 'Y',
-    '--..' => 'Z'
-  }
-return dictionary[character]
+def decode_char(char)
+  dictionary = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C',
+    '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G',
+    '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V',
+    '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z'
+  }.freeze
+  dictionary[char]
 end
 
-def decode_word(givenWord)
-    decodedWord = ''
-    array = givenWord.split
-    array.each do |element|
-      decodedWord += decode_char(element)
-    end
-    return decodedWord
+def decode_word(givenword)
+  decodedword = ''
+  array = givenword.split
+  array.each do |element|
+    decodedword += decode_char(element)
   end
-
-  
+  decodedword
+end
